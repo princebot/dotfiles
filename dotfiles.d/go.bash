3fc631ecf33e9e0eb12a9a7a4265da9adba38192
Prince Williams <prince@princebot.com>
2016-08-31 10:40:47 -0500

    Revise go.bash startup script

--- dotfiles.d/go.bash
+++ dotfiles.d/go.bash
@@ -3,10 +3,20 @@
 # Name:     functions.bash
 # Author:   prince@princebot.com
 # Source:   https://github.com/princebot/dotfiles
-# Synopsis: Set JAVA_HOME for macOS systems.
+# Synopsis: Set golang configuration.
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-which go version &>/dev/null || return
+# If Go is not installed, skip the rest of this script.
+go version &>/dev/null || return
+
+# Set this to the directory you use for your Go workspace; by default, this just
+# assumes it is ~/go if that directory exists.
+if [[ -z ${GOPATH} && -d ~/go ]]; then
+    export GOPATH=~/go
+fi
+
+# If GOPATH is not set, skip the rest of this script.
+[[ -n ${GOPATH} ]] || return
 
 # Change to a directory relative to $GOPATH/src.
 function cdgo { cd "${GOPATH}/src/$1"; }
