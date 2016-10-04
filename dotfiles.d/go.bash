ce75b0ceaed6307ad1d206aea411e34b85fdd199
Prince Williams <prince@princebot.com>
2016-10-04 15:53:52 -0500

    Change $GOPATH to an absolute path.
    Some tools, such as godep, appear to have issues if GOPATH is a
    symbolic link, so I changed go.bash to dereference links before
    setting GOPATH.

--- dotfiles.d/go.bash
+++ dotfiles.d/go.bash
@@ -9,11 +9,15 @@
 # If Go is not installed, skip the rest of this script.
 go version &>/dev/null || return
 
-# Set this to the directory you use for your Go workspace; by default, this just
-# assumes it is ~/go if that directory exists.
+# Set this to your Go workspace directory; by default, this assumes ~/go if
+# that directory exists.
 if [[ -z ${GOPATH} && -d ~/go ]]; then
-    export GOPATH=~/go
-fi
+    __go_path=$(cd ~/go && pwd -P)
+    if [[ -n ${__go_path} ]]; then
+        export GOPATH="${__go_path}"
+        unset -v __go_path
+    fi
+fi 2>/dev/null
 
 # If GOPATH is not set, skip the rest of this script.
 [[ -n ${GOPATH} ]] || return
